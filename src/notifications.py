11f29061f76797d721170261921423b49135ae0d
vvkumar2 <52425114+vvkumar2@users.noreply.github.com>
2023-05-24 12:15:28 -0700

    refactoring

--- src/notifications.py
+++ src/notifications.py
@@ -12,6 +12,7 @@ PUSHOVER_USER_KEY = os.getenv("PUSHOVER_USER_KEY")
 def send_notification(expiration_date_pd=None, strike_price=None, options_amount=None, bid_price=None, profit=None):
     try:
         message = f"SOFI 0{expiration_date_pd.month}/{expiration_date_pd.day}/{expiration_date_pd.year} {strike_price:.2f} P\nSTO {options_amount} @ Limit {bid_price}, Day" if not profit else f"You made a profit of ${profit:.2f} this week! You sold {options_amount} options."
+        # print(message)
         conn = httplib.HTTPSConnection("api.pushover.net:443", context=ssl._create_unverified_context())
         conn.request("POST", "/1/messages.json",
             urllib.urlencode({
